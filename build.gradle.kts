4b8010d65d20fbc93d489c5e0b7114642970fbfc
sentinelweb <sentinelwebtechnologies@gmail.com>
2021-07-13 20:28:26 +0200

    unbypass

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,21 +56,11 @@ kotlin {
     }
 }
 
-//tasks {
-//    val processResources by getting(Copy::class) {
-//        doLast {
-//            buildDir.resolve("js/packages/website/kotlin-dce-dev/secrets.json")
-//                .writeText("""{"SWEBSITE_MAPS_API_KEY" : "${project.properties["SWEBSITE_MAPS_API_KEY"]}" }""")
-//        }
-//    }
-//}
-
-
-//buildDir.resolve("js/packages/website/kotlin-dce/secrets.json")
-//    .writeText("""{"SWEBSITE_MAPS_API_KEY" : "${project.properties["SWEBSITE_MAPS_API_KEY"]}" }""")
-
-//            buildDir.resolve("processedResources/js/main/secrets.json")
-//                .writeText("""{"SWEBSITE_MAPS_API_KEY" : "${project.properties["SWEBSITE_MAPS_API_KEY"]}" }""")
-
-//            buildDir.resolve("processedResources/js/main/secrets.js")
-//                .writeText("""val SWEBSITE_MAPS_API_KEY = "${project.properties["SWEBSITE_MAPS_API_KEY"]}" """)
+tasks {
+    val processResources by getting(Copy::class) {
+        doLast {
+            buildDir.resolve("js/packages/website/kotlin-dce-dev/secrets.json")
+                .writeText("""{"SWEBSITE_MAPS_API_KEY" : "${project.properties["SWEBSITE_MAPS_API_KEY"]}" }""")
+        }
+    }
+}
--- src/main/kotlin/Secrets.kt
+++ src/main/kotlin/Secrets.kt
@@ -1,7 +1,6 @@
-//external interface Secrets {
-//    val SWEBSITE_MAPS_API_KEY: String
-//}
-//external fun require(module:String): dynamic
-//private val secrets: Secrets = require("secrets.json")
-//public val SWEBSITE_MAPS_API_KEY = secrets.SWEBSITE_MAPS_API_KEY
-val SWEBSITE_MAPS_API_KEY="AIzaSyAI3FNELgcqghlfcqKUoLwofnqY5QS1IPg"
+external interface Secrets {
+    val SWEBSITE_MAPS_API_KEY: String
+}
+external fun require(module:String): dynamic
+private val secrets: Secrets = require("secrets.json")
+public val SWEBSITE_MAPS_API_KEY = secrets.SWEBSITE_MAPS_API_KEY
