222635228cb282c780f2ee8e78cb7e83ebba02c0
sentinelweb <robmunro@robs-MacBook-Pro.local>
2024-09-02 01:05:58 +0200

    fix intro resolution

--- composeApp/src/commonMain/kotlin/uk/co/sentinelweb/site/App.kt
+++ composeApp/src/commonMain/kotlin/uk/co/sentinelweb/site/App.kt
@@ -1,9 +1,10 @@
 package uk.co.sentinelweb.site
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.*
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -12,10 +13,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.util.lerp
 import kotlinx.coroutines.delay
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -35,67 +34,43 @@ fun App(sizeProvider: ScreenSizeProvider) {
 @Composable
 fun SplashScreen1(sizeProvider: ScreenSizeProvider) {
     val fullSize = IntSize(sizeProvider.width.toInt(), sizeProvider.height.toInt())
-    val imgSize = IntSize(292, 64)
-    val initialScale = 5f
+    val imgSize = IntSize(1460, 320)
+    val initialScale = 1f
     val animatedOffsetBoundsLeft =
-        remember { Animatable((fullSize.width.toFloat() - imgSize.width) / initialScale ) }
+        remember { Animatable((fullSize.width.toFloat() - imgSize.width) / initialScale) }
     val animatedOffsetBoundsTop =
-        remember { Animatable((fullSize.height.toFloat() - imgSize.height) / initialScale ) }
+        remember { Animatable((fullSize.height.toFloat() - imgSize.height) / initialScale) }
     val animatedOffsetBoundsScale = remember { Animatable(initialScale) }
 
     val animationSpec = tween<Float>(durationMillis = 2000, easing = FastOutSlowInEasing)
 
     val delayStart: Long = 2000
+    val finalScale = 0.2f
+    val finalTopLeft = 10f
     LaunchedEffect(Unit) {
         delay(delayStart)
-        animatedOffsetBoundsLeft.animateTo(targetValue = 10f, animationSpec = animationSpec)
+        animatedOffsetBoundsLeft.animateTo(targetValue = finalTopLeft - imgSize.width + imgSize.width * finalScale, animationSpec = animationSpec)
     }
     LaunchedEffect(Unit) {
         delay(delayStart)
-        animatedOffsetBoundsTop.animateTo(targetValue = 10f, animationSpec = animationSpec)
+        animatedOffsetBoundsTop.animateTo(targetValue = finalTopLeft - imgSize.height + imgSize.height * finalScale, animationSpec = animationSpec)
     }
     LaunchedEffect(Unit) {
         delay(delayStart)
-        animatedOffsetBoundsScale.animateTo(targetValue = 1f, animationSpec = animationSpec)
+        animatedOffsetBoundsScale.animateTo(targetValue = finalScale, animationSpec = animationSpec)
     }
 
     Box(modifier = Modifier.fillMaxSize()) {
         Image(painterResource(Res.drawable.logo_light),
             contentDescription = null,
             modifier = Modifier
-                .scale(animatedOffsetBoundsScale.value) // control the size scaling of your logo
                 .offset {
                     IntOffset(
                         animatedOffsetBoundsLeft.value.roundToInt(),
                         animatedOffsetBoundsTop.value.roundToInt()
                     )
                 }
-
-        )
-    }
-
-
-}
-
-@Composable
-fun SplashScreen() {
-    val transition = rememberInfiniteTransition()
-    val angle by transition.animateFloat(
-        initialValue = 0f,
-        targetValue = 1f,
-        animationSpec = infiniteRepeatable(
-            tween(3000, easing = FastOutSlowInEasing),
-            RepeatMode.Restart
-        )
-    )
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        val offsetX = lerp(0f, -1000f, angle) // substitute -1000f with the actual left side offset you need
-        val offsetY = lerp(0f, -1000f, angle) // substitute -1000f with the actual top side offset you need
-        Image(painterResource(Res.drawable.logo_light),
-            contentDescription = null,
-            modifier = Modifier
-                .scale(lerp(1f, 0.5f, angle)) // change 1f and 0.5f to control the size scaling of your logo
-                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                .scale(animatedOffsetBoundsScale.value) // control the size scaling of your logo
         )
     }
 }
